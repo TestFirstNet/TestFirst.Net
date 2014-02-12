5c573e98a964961508eac0f1a4fda18c8cfab85a
Bert van Brakel <bertvanbrakel+testfirst@gmail.com>
2014-02-12 22:43:34 +1300

    Add Expect.For(..) to provide additional info on failure when used in loops

--- TestFirst.Net/Expect.cs
+++ TestFirst.Net/Expect.cs
@@ -6,12 +6,57 @@ namespace TestFirst.Net {
     /// Conveneince class to use when usign matchers outside of a scenario
     /// </summary>
     public static class Expect {
+        
+        /// <summary>
+        /// Build an expect with a label which is useful in loops when you want to know what the loop value was.
+        /// 
+        /// Usage:
+        ///     
+        /// foreach(var x in bar){
+        ///     Expect.For(x)  -- this is simply printed on failure, not used for matching
+        ///         .That(Something(x))
+        ///         .Is(AFoo.With()...);
+        /// }
+        /// </summary>
+        /// <param name="label"></param>
+        /// <returns></returns>
+        public ExpectWithLabel For(Object label)
+        {
+            return new ExpectWithLabel(label);
+        }
 
+        /// <summary>
+        /// Helper method to make using matchers outside of scenarios easier and cleaner
+        /// 
+        /// Usage:
+        /// var x = Foo();
+        /// Expect.That(x)
+        ///     .Is(AFoo.With()...)
+        ///     
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="actual"></param>
+        /// <returns></returns>
         public static Expect<T> That<T>(T actual) {
             return new Expect<T>(actual);
         }
     }
+    
+    public class ExpectWithLabel
+    {
+        private readonly Object m_label;
 
+        internal ExpectWithLabel(Object label)
+        {
+            m_label = label;
+        }
+
+        public Expect<T> That<T>(T actual)
+        {
+            return new Expect<T>(actual);
+        }
+
+    }
     public class Expect<T> {
         private readonly T m_actual;
 
