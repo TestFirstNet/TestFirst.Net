8a639f145afb20fb0593956137221825140149c6
Bert van Brakel <bertvanbrakel+testfirst@gmail.com>
2014-02-23 22:24:35 +1300

    nuget scripts set nuget package version to the same one as used to tag git

--- nuget.build.sh
+++ nuget.build.sh
@@ -1,16 +1,31 @@
 #!/bin/sh
 
+local_nuget_pkg_dir=~/workspace/local-nuget-repo
+
+read -p "Build as nuget version: " version
+
+echo "Removing old nuget packages"
+find . -name \*.nupkg | xargs -i rm {}
+
 cd TestFirst.Net
-nuget pack TestFirst.Net.csproj  -Symbols
-nuget pack TestFirst.Net.csproj  -IncludeReferencedProjects
+nuget pack TestFirst.Net.csproj  -Symbols -Prop Configuration=Release -Version $version 
+nuget pack TestFirst.Net.csproj  -IncludeReferencedProjects -Prop Configuration=Release -Version $version
 cd ..
 
 cd TestFirst.Net.Extensions
-nuget pack TestFirst.Net.Extensions.csproj  -Symbols
-nuget pack TestFirst.Net.Extensions.csproj  -IncludeReferencedProjects
+nuget pack TestFirst.Net.Extensions.csproj  -Symbols -Prop Configuration=Release -Version $version
+nuget pack TestFirst.Net.Extensions.csproj  -IncludeReferencedProjects -Prop Configuration=Release -Version $version
 cd ..
 
 cd TestFirst.Net.Performance
-nuget pack TestFirst.Net.Performance.csproj  -Symbols
-nuget pack TestFirst.Net.Performance.csproj  -IncludeReferencedProjects
+nuget pack TestFirst.Net.Performance.csproj  -Symbols -Prop Configuration=Release -Version $version
+nuget pack TestFirst.Net.Performance.csproj  -IncludeReferencedProjects -Prop Configuration=Release -Version $version
 cd ..
+
+
+echo ""
+echo "copying packages into local nuget repo dir at $local_nuget_pkg_dir"
+mkdir -p $local_nuget_pkg_dir
+find . -name \*.nupkg | xargs -i cp {} --target $local_nuget_pkg_dir
+
+
--- nuget.release.sh
+++ nuget.release.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+local_nuget_pkg_dir=~/workspace/local-nuget-repo
+
 echo "-------------"
 while true; do
-    read -p "this will build and package TestFirst.Net and then tag git. Continue? yn :" yn
+	echo "this will build and package TestFirst.Net and then tag git"
+	echo "will place a copy of the built packages into $local_nuget_pkg_dir for testing"
+    read -p "Continue? yn :" yn
     case $yn in
         [Yy]* ) 
 			break;;
@@ -10,7 +14,7 @@ while true; do
     esac
 done
 
-read -p "Release as version: " version
+read -p "Release as nuget version: " version
 
 echo "Removing old nuget packages"
 find . -name \*.nupkg | xargs -i rm {}
@@ -18,23 +22,23 @@ find . -name \*.nupkg | xargs -i rm {}
 msbuild.exe TestFirst.Net.sln -t:Clean,Build -p:Configuration=Release
 
 cd TestFirst.Net
-nuget pack TestFirst.Net.csproj  -Symbols -Prop Configuration=Release
-nuget pack TestFirst.Net.csproj  -IncludeReferencedProjects -Prop Configuration=Release
+nuget pack TestFirst.Net.csproj  -Symbols -Prop Configuration=Release -Version $version 
+nuget pack TestFirst.Net.csproj  -IncludeReferencedProjects -Prop Configuration=Release -Version $version
 cd ..
 
 cd TestFirst.Net.Extensions
-nuget pack TestFirst.Net.Extensions.csproj  -Symbols -Prop Configuration=Release
-nuget pack TestFirst.Net.Extensions.csproj  -IncludeReferencedProjects -Prop Configuration=Release
+nuget pack TestFirst.Net.Extensions.csproj  -Symbols -Prop Configuration=Release -Version $version
+nuget pack TestFirst.Net.Extensions.csproj  -IncludeReferencedProjects -Prop Configuration=Release -Version $version
 cd ..
 
 cd TestFirst.Net.Performance
-nuget pack TestFirst.Net.Performance.csproj  -Symbols -Prop Configuration=Release
-nuget pack TestFirst.Net.Performance.csproj  -IncludeReferencedProjects -Prop Configuration=Release
+nuget pack TestFirst.Net.Performance.csproj  -Symbols -Prop Configuration=Release -Version $version
+nuget pack TestFirst.Net.Performance.csproj  -IncludeReferencedProjects -Prop Configuration=Release -Version $version
 cd ..
 
 echo "copying packages into single directory"
-mkdir -p bin/packages
-find . -name \*.nupkg | xargs -i cp {} --target bin/packages/
+mkdir -p $local_nuget_pkg_dir
+find . -name \*.nupkg | xargs -i cp {} --target $local_nuget_pkg_dir
 
 echo 
 echo "-------------"
