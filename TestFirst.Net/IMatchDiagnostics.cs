3675469c58b057e64a267f8811ab03d00c58ff1e
Bert van Brakel <bertvanbrakel+testfirst@gmail.com>
2015-04-10 14:34:50 +0930

    Add some documentation and rename some method args (no api changes)

--- TestFirst.Net/IMatchDiagnostics.cs
+++ TestFirst.Net/IMatchDiagnostics.cs
@@ -2,6 +2,7 @@ using System;
 
 namespace TestFirst.Net
 {
+
     public interface IMatchDiagnostics:IDescription
     {
         IMatchDiagnostics Matched(String text, params Object[] args);
@@ -10,22 +11,68 @@ namespace TestFirst.Net
         IMatchDiagnostics MisMatched(String text, params Object[] args);
         IMatchDiagnostics MisMatched(ISelfDescribing selfDescribing);
 
-        bool TryMatch(Object actual, int index, IMatcher childMatcher);
-        bool TryMatch<T>(T actual, int index, IMatcher<T> childMatcher);
+        /// <summary>
+        /// Use the given matcher to match the given input.  Attempts to pretty format the response
+        /// </summary>
+        /// <returns><c>true</c>, if match was successfull, <c>false</c> otherwise.</returns>
+        /// <param name="actual">Object to match</param>
+        /// <param name="actualIndex">On failure include this as the index for the object</param>
+        /// <param name="childMatcher">The matcher to use against the provided object</param>
+        bool TryMatch(Object actual, int actualIndex, IMatcher childMatcher);
 
+        /// <summary>
+        /// Use the given matcher to match the given input.  Attempts to pretty format the response
+        /// </summary>
+        /// <returns><c>true</c>, if match was successfull, <c>false</c> otherwise.</returns>
+        /// <param name="actual">Object to match</param>
+        /// <param name="actualIndex">On failure include this as the index for the object</param>
+        /// <param name="childMatcher">The matcher to use against the provided object</param>
+        bool TryMatch<T>(T actual, int actualIndex, IMatcher<T> childMatcher);
+
+        /// <summary>
+        /// Use the given matcher to match the given input.  Attempts to pretty format the response
+        /// </summary>
+        /// <returns><c>true</c>, if match was successfull, <c>false</c> otherwise.</returns>
+        /// <param name="actual">Object to match</param>
+        /// <param name="childMatcher">The matcher to use against the provided object</param> 
         bool TryMatch(Object actual, IMatcher childMatcher);
+
+        /// <summary>
+        /// Use the given matcher to match the given input.  Attempts to pretty format the response
+        /// </summary>
+        /// <returns><c>true</c>, if match was successfull, <c>false</c> otherwise.</returns>
+        /// <param name="actual">Object to match</param>
+        /// <param name="childMatcher">The matcher to use against the provided object</param> 
         bool TryMatch<T>(T actual, IMatcher<T> childMatcher);
 
         /// <summary>
-        /// Use the given matcher to match the given inpout.  Attempts to pretty format the response
+        /// Use the given matcher to match the given input.  Attempts to pretty format the response
         /// </summary>
-        /// <returns>if the child matcher matched</returns>
-        bool TryMatch(Object actual, String actualName, IMatcher childMatcher);
-        bool TryMatch<T>(T actual, String actualName, IMatcher<T> childMatcher);
+        /// <returns><c>true</c>, if match was successfull, <c>false</c> otherwise.</returns>
+        /// <param name="actual">Object to match</param>
+        /// <param name="actualLabel">On failure include this as a label for the object</param>
+        /// <param name="childMatcher">The matcher to use against the provided object</param>
+        bool TryMatch(Object actual, String actualLabel, IMatcher childMatcher);
+
+        /// <summary>
+        /// Use the given matcher to match the given input.  Attempts to pretty format the response
+        /// </summary>
+        /// <returns><c>true</c>, if match was successfull, <c>false</c> otherwise.</returns>
+        /// <param name="actual">Object to match</param>
+        /// <param name="actualLabel">On failure include this as a label for the object</param>
+        /// <param name="childMatcher">The matcher to use against the provided object</param>
+        bool TryMatch<T>(T actual, String actualLabel, IMatcher<T> childMatcher);
 
         bool TryMatch(Object actual, IMatcher childMatcher, ISelfDescribing selfDescribing);
         bool TryMatch<T>(T actual, IMatcher<T> childMatcher, ISelfDescribing selfDescribing);
 
+        /// <summary>
+        /// Return a child diagnostics context. This allows for performing sub matches and optionally
+        /// including the match/mismatch messages. 
+        /// 
+        /// The return type is the same type as the current diagnostics. 
+        /// </summary>
+        /// <returns>A new child (or the same instance in the case of a null diagnostics)</returns>
         IMatchDiagnostics NewChild();
     }
 }
