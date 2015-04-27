ba1dcb9485a915f26ce93762ca405d6ffc2bedac
Bert van Brakel <bertvanbrakel+testfirst@gmail.com>
2015-04-27 21:59:46 +1000

    add AKeyValuePair matcher

---
+++ TestFirst.Net/Matcher/AKeyValuePair.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using TestFirst.Net;
+using System.Collections.Generic;
+using TestFirst.Net.Matcher;
+
+namespace TestFirst.Net.Matcher
+{
+    public static class AKeyValuePair{
+
+        public static IMatcher<KeyValuePair<K,V>> EqualTo<K,V>(K key, V value){
+            return EqualTo (AnInstance.EqualTo(key), AnInstance.EqualTo(value));
+        }
+
+        public static IMatcher<KeyValuePair<K,V>> EqualTo<K,V>(K key, IMatcher<V> valueMatcher){
+            return EqualTo (AnInstance.EqualTo(key), valueMatcher);
+        }
+
+        public static IMatcher<KeyValuePair<K,V>> EqualTo<K,V>(IMatcher<K> keyMatcher, IMatcher<V> valueMatcher){
+            return Matchers.Function ((KeyValuePair<K,V> actual,IMatchDiagnostics diag)=>{
+                return diag.TryMatch(actual.Key,keyMatcher) && diag.TryMatch(actual.Value,valueMatcher);
+            },
+            (desc)=>{
+                desc.Text("KeyValuePair");
+                desc.Value("Key", keyMatcher);
+                desc.Text("Value", valueMatcher);
+            });
+        }
+    }
+}
+
