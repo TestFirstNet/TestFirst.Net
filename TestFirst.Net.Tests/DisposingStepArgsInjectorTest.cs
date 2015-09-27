50c216e554aa60f0e3f786f12b0108f7218fecce
Bert van Brakel <bertvanbrakel@gmail.com>
2015-09-27 21:31:56 +1000

    more tests

--- TestFirst.Net.Tests/DisposingStepArgsInjectorTest.cs
+++ TestFirst.Net.Tests/DisposingStepArgsInjectorTest.cs
@@ -8,7 +8,7 @@ using NSubstitute;
 namespace TestFirst.Net.Test
 {
     [TestFixture]
-    public class DisposingtepArgsInjectorTest
+    public class DisposingStepArgsInjectorTest
     {
         [Test]
         public void disposable_is_disposed()
@@ -55,6 +55,27 @@ namespace TestFirst.Net.Test
                 disposable1.Dispose();
             });
         }
+
+        [Test]
+        public void all_are_disposed_even_if_disposable_throws_exception()
+        {
+            var injector = new DisposingStepArgInjector();
+            var disposable1 = Mock<IDisposable>();
+            var disposable2 = Mock<IDisposable>();
+            disposable2.When(_ => _.Dispose()).Do(_ => { throw new Exception(); });
+            var disposable3 = Mock<IDisposable>();
+
+            injector.InjectDependencies(disposable1);
+            injector.InjectDependencies(disposable2);
+            injector.InjectDependencies(disposable3);
+
+            injector.Dispose();
+        
+            disposable1.Received().Dispose();
+            disposable2.Received().Dispose();
+            disposable3.Received().Dispose();
+        }
+
 
         [Test]
         public void nulls_ignored_and_dont_throw_error()
---
+++ TestFirst.Net.Tests/Util/TimesBuilderTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using NSubstitute;
+using Shouldly;
+
+namespace TestFirst.Net.Util
+{
+    [TestFixture]
+    class TimesBuilderTest
+    {
+        [Test]
+        public void test()
+        {
+            var buildFuncReturns = new object();
+            var buildFunc = Substitute.For<Func<int, object>>();
+            buildFunc.Invoke(Arg.Is<int>(3)).Returns<object>(buildFuncReturns);
+
+            var actual = new TimesBuilder<int, object>(3,buildFunc).Times();
+
+            actual.ShouldBeSameAs(buildFuncReturns);
+
+            buildFunc.Received().Invoke(Arg.Is<int>(3));
+        }
+    }
+}
